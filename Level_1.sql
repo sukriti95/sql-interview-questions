1e3665e514fdc753f000fe4d364b4c5cd5b93637
Sukriti Ranjan <sukriti8252@gmail.com>
2026-09-22 16:39:54 +0530

    22 sept

--- Level_1.sql
+++ Level_1.sql
@@ -1,6 +1,14 @@
-create database SQl_inteview_Question;
+-- create database SQl_inteview_Question;
+-- use SQL_interview_Question;
 -- 1.Find the employee with the highest salary.
+Select e.employee_id,e.employee_name,es.salary from employees e join employee_salary es on e.employee_id = es.employee_id
+where es.salary = (select max(salary) from employee_salary);
+
 -- 2.Find the employee with the lowest salary.
+
+select e.employee_id,e.employee_name,s.salary from employees e join employee_salary s on e.employee_id = s.employee_id
+Where s.salary = (select min(salary) from employee_salary);
+
 -- 3.Find the average salary of all employees.
 -- 4.Find the total salary paid to all employees.
 -- 5.Find employees whose salary is greater than the company's average salary.
